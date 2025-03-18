e76ef6caf6240478ad4b6504651ada42f82f4bf6
pragathg <pragathg@uio.no>
2025-03-18 13:12:28 +0100

    feat: added text field to mapScreen

--- app/src/main/java/no/uio/ifi/in2000/team54/ui/map/MapScreen.kt
+++ app/src/main/java/no/uio/ifi/in2000/team54/ui/map/MapScreen.kt
@@ -5,12 +5,18 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -25,6 +31,7 @@ import com.mapbox.maps.extension.compose.MapboxMap
 import com.mapbox.maps.extension.compose.animation.viewport.rememberMapViewportState
 import com.mapbox.maps.extension.compose.annotation.generated.PolygonAnnotation
 import com.mapbox.maps.extension.compose.style.MapStyle
+import com.mapbox.maps.extension.style.expressions.dsl.generated.mod
 import no.uio.ifi.in2000.team54.ui.theme.Team54Theme
 
 
@@ -42,37 +49,54 @@ val ZOOM: Double = 18.0 // Perfekt zoom
 
 @Composable
 fun MapScreen() {
-    MapboxMap(
-    Modifier
-        .fillMaxSize().padding(),
-    mapViewportState = rememberMapViewportState {
-        setCameraOptions {
-            zoom(ZOOM)
-             CAMERA_CENTER
-            center(CAMERA_CENTER)
-        }
-    },
-    style = {
-        MapStyle(style = Style.STANDARD)
-    },
-    ) {
-        PolygonAnnotation(
-            points = POLYGON_POINTS,
+    Box (
+        Modifier.fillMaxSize()
+    ){
+
+        
+        MapboxMap(
+            Modifier
+                .padding(),
+            mapViewportState = rememberMapViewportState {
+                setCameraOptions {
+                    zoom(ZOOM)
+                    CAMERA_CENTER
+                    center(CAMERA_CENTER)
+                }
+            },
+            style = {
+                MapStyle(style = Style.STANDARD)
+            },
         ) {
-            interactionsState.isDraggable = true
-            interactionsState.onClicked {
 
-                interactionsState.isDraggable = false
-                true
-            }
-                .onLongClicked {
 
-                    interactionsState.isDraggable = true
+            PolygonAnnotation(
+                points = POLYGON_POINTS,
+            ) {
+                interactionsState.isDraggable = true
+                interactionsState.onClicked {
+
+                    interactionsState.isDraggable = false
                     true
                 }
-            fillColor = Color.Red
+                    .onLongClicked {
+
+                        interactionsState.isDraggable = true
+                        true
+                    }
+                fillColor = Color.Red
+            }
         }
+
+        TextField(
+            value = "",
+            onValueChange = { },
+            label = { Text("Label") },
+            modifier = Modifier.fillMaxWidth().height(60.dp)
+        )
+
     }
+
 }
 
 
