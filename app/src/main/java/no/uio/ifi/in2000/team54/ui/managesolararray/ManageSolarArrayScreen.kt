25e36f08e28345ed89468aa3c36c1d8c20d2ffc2
amundhil <amundhil@uio.no>
2025-03-20 10:34:18 +0100

    refactor: Set up folder structure, and refactored some files to represent our design

--- app/src/main/java/no/uio/ifi/in2000/team54/ui/managesolararray/ManageSolarArrayScreen.kt
+++ app/src/main/java/no/uio/ifi/in2000/team54/ui/managesolararray/ManageSolarArrayScreen.kt
@@ -1,27 +1,14 @@
-package no.uio.ifi.in2000.team54.ui.map
+package no.uio.ifi.in2000.team54.ui.managesolararray
 
 
-import android.os.Bundle
-import android.widget.Toast
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.ExposedDropdownMenuDefaults
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldColors
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -31,8 +18,6 @@ import com.mapbox.maps.extension.compose.MapboxMap
 import com.mapbox.maps.extension.compose.animation.viewport.rememberMapViewportState
 import com.mapbox.maps.extension.compose.annotation.generated.PolygonAnnotation
 import com.mapbox.maps.extension.compose.style.MapStyle
-import com.mapbox.maps.extension.style.expressions.dsl.generated.mod
-import no.uio.ifi.in2000.team54.ui.theme.Team54Theme
 
 
 val ZOOM: Double = 18.0 // Perfekt zoom
@@ -48,7 +33,7 @@ val ZOOM: Double = 18.0 // Perfekt zoom
     )
 
 @Composable
-fun MapScreen() {
+fun ManageSolarArrayScreen() {
     Box (
         Modifier.fillMaxSize()
     ){
