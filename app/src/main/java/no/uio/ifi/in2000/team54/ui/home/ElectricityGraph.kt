6c430ff20c6eee0892117770fe33f2306d79bf17
Antonina <antonibu@uio.no>
2025-03-22 16:00:29 +0100

    Chore: minor fixes of the grapf size and labels

--- app/src/main/java/no/uio/ifi/in2000/team54/ui/home/ElectricityGraph.kt
+++ app/src/main/java/no/uio/ifi/in2000/team54/ui/home/ElectricityGraph.kt
@@ -1,74 +1,62 @@
 package no.uio.ifi.in2000.team54.ui.home
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import com.patrykandpatrick.vico.compose.cartesian.CartesianChartHost
 import com.patrykandpatrick.vico.compose.cartesian.axis.rememberBottom
 import com.patrykandpatrick.vico.compose.cartesian.axis.rememberStart
-import com.patrykandpatrick.vico.compose.cartesian.cartesianLayerPadding
 import com.patrykandpatrick.vico.compose.cartesian.layer.rememberLineCartesianLayer
 import com.patrykandpatrick.vico.compose.cartesian.rememberCartesianChart
+import com.patrykandpatrick.vico.compose.cartesian.rememberVicoScrollState
 import com.patrykandpatrick.vico.core.cartesian.axis.HorizontalAxis
 import com.patrykandpatrick.vico.core.cartesian.axis.VerticalAxis
 import com.patrykandpatrick.vico.core.cartesian.data.CartesianChartModelProducer
 import com.patrykandpatrick.vico.core.cartesian.data.CartesianValueFormatter
 import com.patrykandpatrick.vico.core.cartesian.data.lineSeries
 import com.patrykandpatrick.vico.core.common.component.TextComponent
-
 @Composable
-fun ElectricityGraph (modifier: Modifier){
+fun ElectricityGraph (modifier: Modifier = Modifier){
     val modelProducer = remember { CartesianChartModelProducer() }
-    val monthFormatter = CartesianValueFormatter { _, value, _ ->
-    val months = listOf(
-        "Jan", "Feb", "Mar", "Apr", "May", "Jun",
-        "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"    )
-    val index = value.toInt().coerceIn(0, months.size - 1)  // Keep within bounds
-    months[index]
+    val monthFormatter = CartesianValueFormatter { _, value, _ -> // overriding "format" method in CastertianValueFormatter
+        val months = listOf(
+            "Jan", "Feb", "Mar", "Apr", "Mai", "Jun",
+            "Jul", "Aug", "Sep", "Okt", "Nov", "Des"
+        )
+        val index = value.toInt().coerceIn(0, months.size - 1)  // Keep within bounds
+        months[index]
     }
-    Box(modifier = Modifier
-        .fillMaxSize(),
-        contentAlignment = Alignment.Center) {
+
+    Box(modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+        ) {
         LaunchedEffect(Unit) {
             modelProducer.runTransaction {
-                lineSeries { series(13, 8, 7, 12, 0, 1, 15, 14, 0, 11, 6, 12) }
+                lineSeries { series(16, 41, 91, 118, 133, 137, 130, 112, 84, 52, 21, 10) }
             }
         }
-
-
         CartesianChartHost(
             rememberCartesianChart(
                 rememberLineCartesianLayer(),
                 startAxis = VerticalAxis.rememberStart(
-                    title = "Electricity (kwM)", titleComponent = TextComponent()
+                    title = "Strøm produsert (kWh)",
+                    titleComponent = TextComponent()
                 ),
                 bottomAxis = HorizontalAxis.rememberBottom(
-                    title = "Time (Months)",
+                    title = "Tid",
                     titleComponent = TextComponent(),
                     valueFormatter = monthFormatter
                 )
             ),
             modelProducer,
-            modifier = Modifier.padding(2.dp)
+            modifier = modifier.padding(16.dp),
+            scrollState = rememberVicoScrollState(scrollEnabled = false) // disabled so it fits the box
         )
-
     }
-
 }
