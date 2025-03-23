d65cbee7849f5f8c21fc7c7e0d2605d73fcf7862
Antonina <antonibu@uio.no>
2025-03-23 17:58:12 +0100

    Feat: added another graph
    Ready for demo

--- app/src/main/java/no/uio/ifi/in2000/team54/MainActivity.kt
+++ app/src/main/java/no/uio/ifi/in2000/team54/MainActivity.kt
@@ -3,10 +3,7 @@ package no.uio.ifi.in2000.team54
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.ui.Modifier
-import no.uio.ifi.in2000.team54.ui.home.ElectricityGraph
-import no.uio.ifi.in2000.team54.ui.managesolararray.ManageSolarArrayScreen
+import no.uio.ifi.in2000.team54.ui.home.TestGraphs
 import no.uio.ifi.in2000.team54.ui.theme.Team54Theme
 
 class MainActivity : ComponentActivity() {
@@ -15,7 +12,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Team54Theme {
                 //ManageSolarArrayScreen()
-                ElectricityGraph(modifier = Modifier)
+                TestGraphs()
             }
         }
     }
