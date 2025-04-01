05ac936132825196da61222b37c2bc8df3dd0955
Radwa Godor <radwaag@uio.no>
2025-04-01 10:41:14 +0200

    feat: added splashscreen

--- app/src/main/java/no/uio/ifi/in2000/team54/MainActivity.kt
+++ app/src/main/java/no/uio/ifi/in2000/team54/MainActivity.kt
@@ -3,10 +3,12 @@ package no.uio.ifi.in2000.team54
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import no.uio.ifi.in2000.team54.ui.theme.Team54Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen().setKeepOnScreenCondition{true}
         super.onCreate(savedInstanceState)
         setContent {
             Team54Theme {
