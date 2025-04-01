e69e8ca848f0abe20066b5b7b91fbcd2d8c75faf
pragathg <pragathg@uio.no>
2025-04-01 18:44:02 +0200

    fix: added distance calculation method in frostData source, tests not perfect

--- app/src/main/java/no/uio/ifi/in2000/team54/data/frost/FrostDatasource.kt
+++ app/src/main/java/no/uio/ifi/in2000/team54/data/frost/FrostDatasource.kt
@@ -11,12 +11,19 @@ import android.util.Base64
 import android.util.Log
 import io.ktor.client.request.header
 import io.ktor.client.statement.HttpResponse
+import io.ktor.client.statement.bodyAsText
 import io.ktor.http.HttpHeaders
 import no.uio.ifi.in2000.team54.domain.Coordinates
 import no.uio.ifi.in2000.team54.model.frost.ObservationData
 import no.uio.ifi.in2000.team54.model.frost.ObservationResponse
 import no.uio.ifi.in2000.team54.model.frost.SensorSystem
 import no.uio.ifi.in2000.team54.model.frost.SourceResponse
+import kotlin.math.atan
+import kotlin.math.atan2
+import kotlin.math.cos
+import kotlin.math.pow
+import kotlin.math.sin
+import kotlin.math.sqrt
 
 class FrostDatasource {
     private val client = HttpClient(CIO) {
@@ -38,35 +45,91 @@ class FrostDatasource {
     private var storedLatitude = 0.0
     private var storedLongitude = 0.0
 
-    private suspend fun fetchNearestSource(coordinates:Coordinates): String {
+    // calculate distance between two coordinates in on a globe:
+    // Haversine formula: https://community.esri.com/t5/coordinate-reference-systems-blog/distance-on-a-sphere-the-haversine-formula/ba-p/902128
+    private suspend fun calculateDistanceBetweenCoords(coordinates1:Coordinates, coordinates2: Coordinates): Double  {
+        val earthRadius = 6371.0 // in km
 
-        val response: HttpResponse = client.get("https://frost.met.no/sources/v0.jsonld?geometry=nearest(POINT(${coordinates.longitude}%20${coordinates.latitude}))") {
-            header(HttpHeaders.Authorization, authHeader)
-            header(HttpHeaders.Accept, "application/json")
+        val distanceBetweenLats = Math.toRadians(coordinates2.latitude - coordinates1.latitude)
+        val distanceBetweenLongs = Math.toRadians(coordinates2.longitude - coordinates1.longitude)
+
+        // a represents the square of half of the chord length between two points on a sphere
+        // helps measure how far apart two points are on the curved surface of the Earth
+        val a = sin(distanceBetweenLats/2).pow(2) + cos(Math.toRadians(coordinates1.latitude)) * cos(Math.toRadians(coordinates2.latitude)) * sin(distanceBetweenLongs/2).pow(2)
+
+        // c is the central angle between the two points which is measured in radians
+        val c = 2* atan2(sqrt(a), sqrt(1 -a))
+
+        return earthRadius * c
+    }
+
+    private suspend fun fetchNearestSource(coordinates:Coordinates, elementName: String? =null): String? {
+
+        if (elementName == null) {
+
+            val response: HttpResponse = client.get("https://frost.met.no/sources/v0.jsonld?geometry=nearest(POINT(${coordinates.longitude}%20${coordinates.latitude}))") {
+                header(HttpHeaders.Authorization, authHeader)
+                header(HttpHeaders.Accept, "application/json")
+            }
+
+            if (response.status.value  != 200) {
+                return null
+            } else {
+                val body: List<SensorSystem> = response.body<SourceResponse>().data
+                return body[0].id
+            }
+
+
+        } else {
+
+            Log.i("testingNearest", "Im here")
+
+            val response: HttpResponse = client.get("https://frost.met.no/sources/v0.jsonld?elements=$elementName") {
+                header(HttpHeaders.Authorization, authHeader)
+                header(HttpHeaders.Accept, "application/json")
+            }
+            println(elementName.toString())
+            println(response.bodyAsText())  // Log the actual response
+
+
+            val body: List<SensorSystem> = response.body<SourceResponse>().data
+            var nearestDistance = 0.0
+            var nextNearestSensor = ""
+
+
+            body.forEach { sensor ->
+                val calcDist = calculateDistanceBetweenCoords(Coordinates(sensor.geometry.coordinates[0], sensor.geometry.coordinates[1]), coordinates)
+                if (nearestDistance == 0.0) {
+                    nearestDistance = calcDist
+                    nextNearestSensor = sensor.id
+                } else if (calcDist < nearestDistance)  {
+                    nearestDistance = calcDist
+                    nextNearestSensor = sensor.id
+                }
+            }
+            return nextNearestSensor
         }
 
-        val body: List<SensorSystem> = response.body<SourceResponse>().data
-
-        return body[0].id
     }
+
 
     suspend fun fetchObservationDataFromFrost(
         coordinates: Coordinates,
         elementName: String,
         referenceTime: String
     ): List<ObservationData> {
-        if(sensorId == "") {
-            sensorId = fetchNearestSource(coordinates)
-            storedLatitude = coordinates.latitude
-            storedLongitude = coordinates.longitude
-        }
-        sensorId = "SN18700"
+        if(sensorId == "" || coordinates.latitude != storedLatitude || coordinates.longitude != storedLongitude) {
 
-        if (coordinates.latitude != storedLatitude || coordinates.longitude != storedLongitude) { // coordiantes changed, so sensor needs to be updated.
-            sensorId = fetchNearestSource(coordinates)
+           if (fetchNearestSource(coordinates) ==  null) {
+               sensorId = fetchNearestSource(coordinates, elementName).toString()
+           } else {
+               sensorId = fetchNearestSource(coordinates).toString()
+           }
             storedLatitude = coordinates.latitude
             storedLongitude = coordinates.longitude
         }
+       // sensorId = "SN18700"
+
 
         var url = "https://frost.met.no/observations/v0.jsonld?sources=$sensorId&referencetime=$referenceTime&elements=$elementName"
 
@@ -80,9 +143,12 @@ class FrostDatasource {
             header(HttpHeaders.Authorization, authHeader)
             header(HttpHeaders.Accept, "application/json")
         }
+        println(response.bodyAsText())  // Log the actual response
 
         val observationResponse: ObservationResponse = response.body()
 
         return observationResponse.data
+
+
     }
 }
