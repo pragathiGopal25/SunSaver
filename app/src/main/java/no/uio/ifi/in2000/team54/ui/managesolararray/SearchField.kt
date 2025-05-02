ec244736ac506eb4b29ae90393db93378423159e
Alex <alexwith03@gmail.com>
2025-05-02 17:39:51 +0200

    chore: remove debug

--- app/src/main/java/no/uio/ifi/in2000/team54/ui/managesolararray/SearchField.kt
+++ app/src/main/java/no/uio/ifi/in2000/team54/ui/managesolararray/SearchField.kt
@@ -86,7 +86,6 @@ fun SearchField(
     }
     val selectSuggestion: (Address) -> Unit = remember {
         { suggestion ->
-            println("set search address to ${suggestion.toFormatted()}")
             viewModel.setSearchAddress(suggestion.toFormatted())
             viewModel.setMapAddress(suggestion)
             scope.launch {
