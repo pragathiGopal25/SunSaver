0bf210fa9c14cfaa848d1ad1e4f8ff94ca5860e5
Radwa Godor <radwaag@uio.no>
2025-05-06 02:33:54 +0200

    choir: removed unnecessary imports from InforScreen

--- app/src/main/java/no/uio/ifi/in2000/team54/ui/info/InfoScreen.kt
+++ app/src/main/java/no/uio/ifi/in2000/team54/ui/info/InfoScreen.kt
@@ -1,7 +1,6 @@
 package no.uio.ifi.in2000.team54.ui.info
 
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -12,7 +11,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -23,7 +21,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
